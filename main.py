e89959e58c5c7decccde7b3badd0baa698113a63
N11 <tib@tib.cz>
2021-06-07 17:18:43 +0200

    Updated to 1.6.

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
 import pygame
 
-mapa = []
-
-class player:
-    def __init__(self):
-        coord = (1, 1)
-
-
 def init():
-    mapa = [
+    map = [
        [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
        [2, 5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2],
        [2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2],
@@ -25,3 +18,38 @@ def init():
     pygame.init()
 
 init()
+
+pygame.init()
+
+window = pygame.display.set_mode((1200, 600))
+
+empty = pygame.image.load("img/1.png")
+wall = pygame.image.load("img/2.png")
+money = pygame.image.load("img/3.png")
+finish = pygame.image.load("img/4.png")
+
+character = pygame.image.load("img/character.png")
+character_x = 1
+character_y = 10
+
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    for y in range(0, 12, 1):
+        for x in range(0, 24, 1):
+            space = map[y][x]
+            if space == 1:
+                window.blit(empty, (x * 50, y * 50))
+            elif space == 2:
+                window.blit(wall, (x * 50, y * 50))
+            elif space == 3:
+                window.blit(money, (x * 50, y * 50))
+            elif space == 4:
+                window.blit(finish, (x * 50, y * 50))
+
+    window.blit(character, (character_x * 50, character_y * 50))
+
+    pygame.display.flip()

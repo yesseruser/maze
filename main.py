eea73f533ad02df5fe064591bdf7122c71989fd5
N11 <tib@tib.cz>
2021-06-07 17:23:17 +0200

    Added movement

--- main.py
+++ main.py
@@ -32,23 +32,32 @@ character = pygame.image.load("img/character.png")
 character_x = 1
 character_y = 10
 
+for y in range(0, 12, 1):
+    for x in range(0, 24, 1):
+        space = map[y][x]
+        if space == 1:
+            window.blit(empty, (x * 50, y * 50))
+        elif space == 2:
+            window.blit(wall, (x * 50, y * 50))
+        elif space == 3:
+            window.blit(money, (x * 50, y * 50))
+        elif space == 4:
+            window.blit(finish, (x * 50, y * 50))
+
 run = True
 while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-
-    for y in range(0, 12, 1):
-        for x in range(0, 24, 1):
-            space = map[y][x]
-            if space == 1:
-                window.blit(empty, (x * 50, y * 50))
-            elif space == 2:
-                window.blit(wall, (x * 50, y * 50))
-            elif space == 3:
-                window.blit(money, (x * 50, y * 50))
-            elif space == 4:
-                window.blit(finish, (x * 50, y * 50))
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                character_x += 1
+            elif event.key == pygame.K_LEFT:
+                character_x -= 1
+            elif event.key == pygame.K_DOWN:
+                character_y += 1
+            elif event.key == pygame.K_UP:
+                character_y -= 1
 
     window.blit(character, (character_x * 50, character_y * 50))
 
